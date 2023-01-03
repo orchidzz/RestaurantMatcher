9e52b2e6f9f3b642159ddffc4f34d875913963c4
Hang Nguyen <lannguyenhang@gmail.com>
2023-01-03 14:58:32 -0800

    done with design

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RestaurantMatcher
+
+Automate the process of choosing a restaurant by matching user with a restaurant based on user's preferences inputs (similar to dating apps but for restaurant)
---
+++ index.js
@@ -0,0 +1,14 @@
+$("document").ready(function () {
+    $("#filter-btn").click(function () {
+        $("#card-collapse").collapse("hide");
+    });
+    $("#show-btn").click(function () {
+        $("#input-collapse").collapse("hide");
+    });
+
+    //verify address
+
+    //call yelp api
+
+    //store result and present cards if no changes are made in filter form
+});
