f6687cd74fc18ab8723016b2ce9def473bc6be61
Hang Nguyen <lannguyenhang@gmail.com>
2023-01-09 13:56:21 -0800

    finished

---
+++ app.js
@@ -0,0 +1,175 @@
+const express = require("express");
+const axios = require("axios");
+const nodecache = require("node-cache");
+require("dotenv").config();
+const cache = new nodecache({ stdTTL: 3599 });
+const app = express();
+
+app.use(express.static(__dirname));
+cache.mset([
+    { key: "restaurants", val: [] },
+    { key: "location", val: "" },
+    { key: "coords", val: [] },
+    { key: "radius", val: "" },
+    { key: "foodTypes", val: "" },
+    { key: "placeTypes", val: "" },
+    { key: "prices", val: "" },
+]);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/getRestaurant", async (req, res) => {
+    if (req.query.location != cache.get("location")) {
+        cache.set("location", req.query.location);
+        await geocode(cache.get("location"));
+        await callYelpApi(
+            cache.get("coords"),
+            req.query.radius,
+            req.query.foodTypes,
+            req.query.placeTypes,
+            req.query.prices
+        );
+    } else if (
+        req.query.radius != cache.get("radius") ||
+        req.query.foodTypes != cache.get("foodTypes") ||
+        req.query.placeTypes != cache.get("placeTypes") ||
+        req.query.prices != cache.get("prices")
+    ) {
+        await callYelpApi(
+            cache.get("coords"),
+            req.query.radius,
+            req.query.foodTypes,
+            req.query.placeTypes,
+            req.query.prices
+        );
+    }
+    if (cache.get("restaurants").length != 0) {
+        var restaurants = cache.get("restaurants");
+        var restaurant = restaurants.pop();
+        cache.set("restaurants", restaurants);
+        res.json(JSON.stringify(restaurant));
+    } else {
+        res.json();
+    }
+});
+
+async function geocode(location) {
+    try {
+        if (!process.env.GEOCODE_API_KEY) {
+            throw new Error("You forgot to set GEOCODE_API_KEY");
+        }
+        response = await axios({
+            url: "https://api.geoapify.com/v1/geocode/search",
+            method: "get",
+            params: {
+                text: location,
+                limit: 1,
+                apiKey: process.env.GEOCODE_API_KEY,
+            },
+        });
+
+        result = response.data.features[0].properties;
+        cache.set("coords", [result.lon, result.lat]);
+    } catch (error) {
+        console.log("error geocoding address", error);
+    }
+}
+
+async function callYelpApi(
+    givenCoords,
+    givenRadius,
+    givenFoodTypes,
+    givenPlaceTypes,
+    givenPrices
+) {
+    try {
+        if (!process.env.YELP_API_KEY) {
+            throw new Error("You forgot to set YELP_API_KEY");
+        }
+        const config = {
+            Authorization: "Bearer" + process.env.YELP_API_KEY,
+            Accept: "application/json",
+        };
+        cache.set("coords", givenCoords);
+        cache.set("radius", givenRadius);
+        cache.set("foodTypes", givenFoodTypes);
+        cache.set("placeTypes", givenPlaceTypes);
+        cache.set("prices", givenPrices);
+        var givenCatergories;
+        if (givenFoodTypes == "") {
+            givenCatergories = givenPlaceTypes;
+        } else if (givenPlaceTypes == "") {
+            givenCatergories = givenFoodTypes;
+        } else {
+            givenCatergories = givenFoodTypes + "," + givenPlaceTypes;
+        }
+
+        response = await axios({
+            url: "https://api.yelp.com/v3/businesses/search",
+            method: "get",
+            params: {
+                longitude: givenCoords[0],
+                latitude: givenCoords[1],
+                radius: givenRadius,
+                price: givenPrices,
+                catergories: givenCatergories,
+                limit: 50,
+                sort_by: "best_match",
+            },
+            headers: config,
+        });
+        var restaurants = new Array();
+        var businesses = response.data.businesses;
+        for (var business of businesses) {
+            var restaurant = new Restaurant();
+            restaurant.url = business.url;
+            restaurant.imgUrl = business.image_url;
+            restaurant.name = business.name;
+            restaurant.rating = business.rating;
+            restaurant.url = business.url;
+            restaurant.price = business.price;
+            restaurant.location = business.location.display_address;
+            restaurant.reviewCount = business.review_count;
+            restaurants.push(restaurant);
+            cache.set("restaurants", restaurants);
+        }
+        cache.set("restaurants", restaurants);
+    } catch (error) {
+        console.log("error calling yelp api", error);
+    }
+}
+
+class Restaurant {
+    url = "";
+    imgUrl = "";
+    name = "";
+    price;
+    rating;
+    reviewCount;
+    location;
+}
+app.listen(3000, () => {
+    console.log("started");
+});
+// overwrite equality method of array type
+// adapted from: https://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript
+// Array.prototype.equals = function (array) {
+//     // if the other array is a falsy value, return
+//     if (!array) return false;
+//     // compare lengths - can save a lot of time
+//     if (this.length != array.length) return false;
+
+//     for (var i = 0; i < this.length(); ++i) {
+//         if (!array.includes(this[i])) {
+//             return false;
+//         }
+//     }
+//     for (var i = 0; i < array.length(); ++i) {
+//         if (!this.includes(array[i])) {
+//             return false;
+//         }
+//     }
+//     return true;
+// };
--- index.js
+++ index.js
@@ -1,14 +1,100 @@
 $("document").ready(function () {
-    $("#filter-btn").click(function () {
+    $("#filter-btn").on("click", function () {
         $("#card-collapse").collapse("hide");
     });
-    $("#show-btn").click(function () {
-        $("#input-collapse").collapse("hide");
+    $("#show-btn").on("click", function () {
+        $("#reject-restaurant").click();
     });
 
-    //verify address
+    $("#reject-restaurant").on("click", function () {
+        // show a restaurant if available
+        $("#input-collapse").collapse("hide");
+        var prices = [];
+        if ($("#prices input:checked").length == 0) {
+            alert("Need to check at least one price");
+            return;
+        }
+        $("#prices input:checked").each(function () {
+            if ($(this).attr("id") == "cheap") {
+                prices.push(1);
+            } else if ($(this).attr("id") == "affordable") {
+                prices.push(2);
+            } else if ($(this).attr("id") == "medium") {
+                prices.push(3);
+            } else {
+                prices.push(4);
+            }
+        });
 
-    //call yelp api
+        if ($("#location").val() != "") {
+            req_location = $("#location").val();
+            // 1 mile = 1609 meters
+            req_radius = $("#distance").val() * 1609;
+            req_foodTypes = $("#food-type").val();
+            req_placeTypes = $("#place-type").val();
 
-    //store result and present cards if no changes are made in filter form
+            ///parse strings of foodTypes and placeTypes into strings separated by commas
+            req_foodTypes = req_foodTypes.replace(" ", "").toLowerCase();
+            req_placeTypes = req_placeTypes.replace(" ", "").toLowerCase();
+            $.ajax({
+                url: "http://localhost:3000/getRestaurant",
+                contentType: "application/json",
+                type: "GET",
+                data: {
+                    location: req_location,
+                    prices: prices.toString(),
+                    foodTypes: req_foodTypes,
+                    placeTypes: req_placeTypes,
+                    radius: req_radius,
+                },
+                success: function (response) {
+                    if (response != null) {
+                        response = JSON.parse(response);
+                        $("#restaurant-name").text(response.name);
+                        $("#restaurant-location").text(response.location);
+                        $("#restaurant-img").attr("src", response.imgUrl);
+                        $("#restaurant-rating-count").text(
+                            response.reviewCount
+                        );
+                        $("#restaurant-price").text(response.price);
+                        $("#choose-restaurant").attr("href", response.url);
+
+                        for (i = 1; i < response.rating; ++i) {
+                            $("#restaurant-rating" + i).attr(
+                                "class",
+                                "bi bi-star-fill"
+                            );
+                        }
+                        if ((response.rating / 0.5) % 2 == 1) {
+                            $(
+                                "#restaurant-rating" + response.rating + 0.5
+                            ).attr("class", "bi bi-star-half");
+                        }
+                        $("#error-div").css("display", "none");
+                        $("#result-div").css("display", "block");
+                    } else {
+                        // hide the card or show some warning/error
+                        $("#error-div").css("display", "block");
+                        $("#result-div").css("display", "none");
+                    }
+                },
+                error: function (error) {
+                    console.log("error getting restaurants: ", error);
+                    // hide the card or show some warning/error
+                    $("#error-div").css("display", "block");
+                    $("#result-div").css("display", "none");
+                },
+            });
+        } else {
+            // hide the card or show some warning/error
+            $("#error-div").css("display", "block");
+            $("#result-div").css("display", "none");
+        }
+    });
+
+    $("#choose-restaurant").on("click", function (e) {
+        e.preventDefault();
+        // direct to yelp page of the restaurant chosen
+        window.open($("#choose-restaurant").attr("href"), "_blank");
+    });
 });
